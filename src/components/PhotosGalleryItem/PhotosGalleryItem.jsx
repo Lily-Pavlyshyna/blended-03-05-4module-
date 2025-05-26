f509f18a9510c1a3d988c8cfa45a77d28f6a2112
Lily Pavlyshyna <pavlisinalilia869@gmail.com>
2025-05-26 21:35:03 +0300

    task-2 done

--- src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
@@ -1,4 +1,17 @@
-const PhotosGalleryItem = () => {
-  return <h3>PhotosGalleryItem</h3>;
+import styles from './PhotosGalleryItem.module.css';
+const PhotosGalleryItem = ({ alt, avg_color, src, openModal }) => {
+  console.log(src.large, alt);
+  return (
+    <div
+      className={styles.thumb}
+      style={{ backgroundColor: avg_color, borderColor: avg_color }}
+    >
+      <img
+        src={src.large}
+        alt={alt}
+        onClick={() => openModal(src.large, alt)}
+      />
+    </div>
+  );
 };
 export default PhotosGalleryItem;
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,91 @@
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
-
+import { useEffect, useState } from 'react';
+import { getPhotos } from '../apiService/photos';
+import PhotosGallery from '../components/PhotosGallery/PhotosGallery';
+import Loader from '../components/Loader/Loader';
+import Button from '../components/Button/Button';
+import { ImageModal } from '../components/ImageModal/ImageModal';
 const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalSrc, setModalSrc] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
+  useEffect(() => {
+    if (!query) return;
+    const fetchImages = async () => {
+      setIsLoading(true);
+      try {
+        const { total_results, photos, per_page } = await getPhotos(
+          query,
+          page
+        );
+        if (!photos.length) {
+          return setIsEmpty(true);
+        }
+        setImages(prevImages => [...prevImages, ...photos]);
+        setIsVisible(page < Math.ceil(total_results / per_page));
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchImages();
+  }, [page, query]);
+
+  const onHandleSubmit = value => {
+    setQuery(value);
+    setImages([]);
+    setPage(1);
+    setError(null);
+    setIsEmpty(false);
+    setIsVisible(false);
+  };
+  const onLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+  const openModal = (alt, src) => {
+    setIsOpen(true);
+    setModalSrc(src);
+    setModalAlt(alt);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+    setModalSrc('');
+    setModalAlt('');
+  };
   return (
     <>
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onHandleSubmit} />
+      {images.length > 0 && (
+        <PhotosGallery images={images} openModal={openModal} />
+      )}
+      {isVisible && (
+        <Button onClick={onLoadMore} disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Load More'}
+        </Button>
+      )}
+      {!images.length && !isEmpty && !error && (
+        <Text textAlign="center">Let`s begin search 🔎</Text>
+      )}
+      {isLoading && <Loader />}
+      {error && <Text textAlign="center">❌Something went wrong</Text>}
+      {isEmpty && (
+        <Text textAlign="center">Sorry, there are no any images... 😭</Text>
+      )}
+      <ImageModal
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+        src={modalSrc}
+        alt={modalAlt}
+      />
     </>
   );
 };
