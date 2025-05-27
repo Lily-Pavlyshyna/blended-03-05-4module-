65ce4cdfc7391a8c96ddeb7138640b759d702433
Lily Pavlyshyna <pavlisinalilia869@gmail.com>
2025-05-27 22:48:58 +0300

    task-1 done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,36 +1,63 @@
 import { FiSearch } from 'react-icons/fi';
 import style from './Form.module.css';
 import { useState } from 'react';
-const Form = ({ onSubmit }) => {
-  const [query, setQuery] = useState('');
-  const handleChange = e => {
-    setQuery(e.target.value);
-  };
+// const Form = ({ onSubmit }) => {
+//   const [query, setQuery] = useState('');
+//   const handleChange = e => {
+//     setQuery(e.target.value);
+//   };
+//   console.log(query);
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     if (!query.trim()) {
+//       return alert('Can not be empty');
+//     }
+//     onSubmit(query);
+//     setQuery('');
+//
+//   };
+//   return (
+//     <form className={style.form} onSubmit={handleSubmit}>
+//       <button className={style.button} type="submit">
+//         <FiSearch size="16px" />
+//       </button>
+
+//       <input
+//         className={style.input}
+//         placeholder="What do you want to write?"
+//         name="search"
+//         required
+//         autoFocus
+//         value={query}
+//         onChange={handleChange}
+//       />
+//     </form>
+//   );
+// };
+
+// export default Form;
+export default function Form({ onSubmit }) {
   const handleSubmit = e => {
     e.preventDefault();
-    if (!query.trim()) {
-      return alert('Can not be empty');
+    const value = e.target.elements.search.value.trim();
+    if (value) {
+      onSubmit(value);
+      e.target.reset();
     }
-    onSubmit(query);
-    setQuery('');
   };
+
   return (
     <form className={style.form} onSubmit={handleSubmit}>
       <button className={style.button} type="submit">
         <FiSearch size="16px" />
       </button>
-
       <input
         className={style.input}
         placeholder="What do you want to write?"
         name="search"
         required
         autoFocus
-        value={query}
-        onChange={handleChange}
       />
     </form>
   );
-};
-
-export default Form;
+}
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,34 @@
 import Text from '../components/Text/Text';
-
+import Form from '../components/Form/Form';
+import { useState, useEffect } from 'react';
+import inTodos from '../components/todos.json';
+import TodoList from '../components/TodoList/TodoList';
+import { nanoid } from 'nanoid';
 const Todos = () => {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  const addNewTodos = text => {
+    const newTodo = { id: nanoid(), text };
+    setTodos(prev => [newTodo, ...prev]);
+  };
+  const deleteTodo = todoId => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(el => el.id !== todoId);
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <>
+      <Form onSubmit={addNewTodos} />
       <Text textAlign="center">There are no any todos ...</Text>
+      <TodoList onDelete={deleteTodo} todos={todos} />
     </>
   );
 };
